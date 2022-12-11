366d86805353dbef56193c8ada03d9401fcd1062
Thosam1 <thosamtenzin2002@gmail.com>
2022-12-11 10:49:09 +0100

    building navigation tree

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -2,11 +2,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+// navigation
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// screens
+import CustomersScreen from '../screens/CustomersScreen';
+import OrdersScreen from '../screens/OrdersScreen';
+
+
+const Tab = createBottomTabNavigator();
+
 const TabNavigator = () => {
   return (
-    <View>
-      <Text>TabNavigator</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Customers" component={CustomersScreen} />
+      <Tab.Screen name="Orders" component={OrdersScreen} />
+    </Tab.Navigator>
   )
 }
 
