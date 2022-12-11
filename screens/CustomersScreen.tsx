659f9d1c33ceff9743afc2b96ff8d9170048f0f2
Thosam1 <thosamtenzin2002@gmail.com>
2022-12-11 12:03:10 +0100

    CustomersScreen basic layout done

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,15 +1,55 @@
 // rnfe snippet
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import { Text, ScrollView, ActivityIndicator } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+
+// Styles
 import { useTailwind } from "tailwind-rn";
+import { Image, Input } from "@rneui/themed";
+
+// navigation
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { TabStackParamList } from "../navigator/TabNavigator";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+// type definitions
+export type CustomerScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Customers">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 const CustomersScreen = () => {
   const tw = useTailwind();
+  const navigation = useNavigation<CustomerScreenNavigationProp>();
+
+  const [input, setInput] = useState<string>();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      // hiding the header title
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <SafeAreaView>
-      <Text style={tw("text-blue-500")}>CustomersScreen</Text> 
-    </SafeAreaView>
+    <ScrollView style={{ backgroundColor: "#59C1CC" }}>
+      <Image
+        source={{ uri: "https://links.papareact.come/3jc" }}
+        containerStyle={tw("w-full h-64")}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+
+      <Input
+        placeholder="Search by Customer"
+        value={input}
+        onChangeText={setInput} // setInput = (text) => setInput(text)
+        containerStyle={tw("bg-white pt-5 pb-0 px-10")}
+      />
+    </ScrollView>
   );
 };
 
