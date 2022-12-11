1b899de18970af6b4b39f1bf1d46fb3143b22719
Thosam1 <thosamtenzin2002@gmail.com>
2022-12-11 13:39:48 +0100

    apollo client initialization

--- App.tsx
+++ App.tsx
@@ -11,13 +11,26 @@ import { NavigationContainer } from '@react-navigation/native';
 //components
 import RootNavigator from './navigator/RootNavigator';
 
+// apollo client
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const PRODUCTION = false;
+const isProductionURL = PRODUCTION ? "https://stmartin.stepzen.net/api/soft-sponge/__graphql" : "http://localhost:5001/api/soft-sponge";
+
+const client = new ApolloClient({
+  uri: isProductionURL,
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>      
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>      
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
