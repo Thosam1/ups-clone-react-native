9ba9c115d9fb9d75655a73a74ea94033d09dc969
Thosam1 <thosamtenzin2002@gmail.com>
2022-12-11 10:49:00 +0100

    Building navigation tree

--- App.tsx
+++ App.tsx
@@ -9,24 +9,15 @@ import utilities from './tailwind.json';
 import { NavigationContainer } from '@react-navigation/native';
 
 //components
-import CustomersScreen from './screens/CustomersScreen';
+import RootNavigator from './navigator/RootNavigator';
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
       <NavigationContainer>      
-        <CustomersScreen />
+        <RootNavigator />
       </NavigationContainer>
     </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
