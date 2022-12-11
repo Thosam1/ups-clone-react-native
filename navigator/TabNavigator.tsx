f9100d416de7c0dfb0dd0c8f4565e389fec3e680
Thosam1 <thosamtenzin2002@gmail.com>
2022-12-11 11:43:00 +0100

    TabNavigator with color and icon switch

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,40 +1,65 @@
 /* rnfe - TabNavigator has 2 tabs: Customers and Orders -> can be changed in the bottom of the screen */
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect } from "react";
 
 // navigation
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 
 // screens
-import CustomersScreen from '../screens/CustomersScreen';
-import OrdersScreen from '../screens/OrdersScreen';
+import CustomersScreen from "../screens/CustomersScreen";
+import OrdersScreen from "../screens/OrdersScreen";
+
+// icons
+import { Icon } from "@rneui/themed";
 
 // type definitions
 export type TabStackParamList = {
-    // will check if the names param are correct
-    Customers: undefined;
-    Orders: undefined;
-}
+  // will check if the names param are correct
+  Customers: undefined;
+  Orders: undefined;
+};
 
 const Tab = createBottomTabNavigator<TabStackParamList>();
 
 const TabNavigator = () => {
-
-    // removing the "Main" header from the RootNavigator
-    const navigation = useNavigation();
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false,
-        });
-    }, []);
-
+  // removing the "Main" header from the RootNavigator
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Customers") {
+            return (
+              <Icon
+                name="users"
+                type="entypo"
+                color={focused ? "#59C1CC" : "gray"}
+              />
+            );
+          } else if (route.name === "Orders") {
+            return (
+              <Icon
+                name="box"
+                type="entypo"
+                color={focused ? "#EB6A7C" : "gray"}
+              />
+            );
+          }
+        },
+      })}
+    >
       <Tab.Screen name="Customers" component={CustomersScreen} />
       <Tab.Screen name="Orders" component={OrdersScreen} />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
-export default TabNavigator
+export default TabNavigator;
